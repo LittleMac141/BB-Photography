de5c3aaed8e747764eeacc9024233e42ef202264
LittleMac141 <156053230+LittleMac141@users.noreply.github.com>
2024-07-02 20:15:32 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSideBar() {
+    const sideBar = document.getElementById('#moblie')
+    sideBar.style.display = 'flex'
+}
+
+function hideSideBar() {
+    const sideBar = document.getElementById('#moblie')
+    sideBar.style.display = 'flex'
+}
